6db126d67a78ba56bcb21ab4cb93e7cc2e3dc3fc
saif-abualkhair <saif_abualkhair@hotmail.com>
2024-01-06 21:16:20 +0300

    advanced live events chart done

--- src/app/modules/home/live-events-advanced/live-events-advanced.component.ts
+++ src/app/modules/home/live-events-advanced/live-events-advanced.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'rd-live-events-advanced',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./live-events-advanced.component.scss']
 })
 export class LiveEventsAdvancedComponent {
+  chart!: Chart;
 
+  ngOnInit() {
+    const ctx = (document.getElementById('liveEventsAdvancedChart') as HTMLCanvasElement)?.getContext('2d');
+
+    new Chart(ctx!, {
+      type: 'doughnut',
+      data: {
+        labels: ['Primary', 'Amber', 'Red'],
+        datasets: [{
+          data: [841, 270, 200],
+          backgroundColor: ['#5991e2', '#da7739', '#bc4c34'],
+          borderWidth: 0
+        }]
+      },
+      options: {
+        cutout: '70%',
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            callbacks: {
+              label: (context) => {
+                return '';
+              },
+              title: (context) => {
+                if (context.length > 0) {
+                  return context[0].raw as string;
+                }
+                return '';
+              },
+            },
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+          }
+        }
+      },
+    });
+  }
 }
